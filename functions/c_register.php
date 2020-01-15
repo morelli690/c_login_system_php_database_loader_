f81a6aa17b9124e0cd458248c1cb3f68f3ef102f
Finguimbrine <50637180+Finguimbrine@users.noreply.github.com>
2020-01-14 20:36:23 -0400

    ravioli

--- functions/c_register.php
+++ functions/c_register.php
@@ -7,11 +7,11 @@ function c_register($c_username, $c_email, $c_password) {
     global $c_con; // ghetto asf(i dont like funcs!)
 
     if (!empty($c_username) && !empty($c_password) && !empty($c_email)) {
-        $c_user_result = "SELECT * FROM `c_data` WHERE c_username='" . mysqli_real_escape_string($c_con, $c_username) . "'";
+        $c_user_result = "SELECT * FROM c_data WHERE c_username='" . mysqli_real_escape_string($c_con, $c_username) . "'";
         $c_user_check = $c_con->query($c_user_result);
         if (!mysqli_num_rows($c_user_check) > 0) {
             $c_email_check = "SELECT * FROM c_data WHERE c_email = '" . mysqli_real_escape_string($c_con, $c_email) . "'";
-            $c_email_result = mysqli_query($c_con, $c_email_check);
+            $c_email_result = $c_con->query($c_email_check);
             if (!mysqli_num_rows($c_email_result) > 0) {
                 $c_enc_pass = password_hash($c_password, PASSWORD_BCRYPT);
                 $c_ip = file_get_contents("http://api.ipify.org"); // ghetto asf(i dont like funcs!)
