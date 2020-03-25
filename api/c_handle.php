0afdcdca6912f45f81cdb7862010d8398c43d75f
Finguimbrine <50637180+Finguimbrine@users.noreply.github.com>
2020-03-24 22:41:34 -0300

    security 1

--- api/c_handle.php
+++ api/c_handle.php
@@ -2,7 +2,7 @@
 include ("../functions/c_main.php");
 
 if(isset($_GET["m"])) {
-    if (c_api_login($_GET["username"], $_GET["password"], $_GET["hwid"])) {
+    if (c_api_login(c_security::decrypt($_POST["username"]), c_security::decrypt($_POST["password"]), c_security::decrypt($_POST["hwid"]))) {
         echo c_security::encrypt(c_response::$c_api_login);
         exit();
     } else {
